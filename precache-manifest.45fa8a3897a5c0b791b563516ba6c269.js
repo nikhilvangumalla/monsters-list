41e0ee823bf25675b399ac8d19a72c5cb781536e
Nikhil Vangumalla <nikhilvangumalla@gmail.com>
2020-01-10 21:51:50 +0530

    Updates

---
+++ precache-manifest.45fa8a3897a5c0b791b563516ba6c269.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bea7cd3f0942f108283a7df6afcbf419",
+    "url": "/monsters-list/index.html"
+  },
+  {
+    "revision": "09e1c05777e2b52cffdb",
+    "url": "/monsters-list/static/css/main.80b05415.chunk.css"
+  },
+  {
+    "revision": "739181bd8fcd6e4f8cda",
+    "url": "/monsters-list/static/js/2.09ccd71b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-list/static/js/2.09ccd71b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09e1c05777e2b52cffdb",
+    "url": "/monsters-list/static/js/main.bd15776b.chunk.js"
+  },
+  {
+    "revision": "ed0eae5f2dc89cc82284",
+    "url": "/monsters-list/static/js/runtime-main.39fb16b6.js"
+  }
+]);
